0923c0b02c3e9c7647246c2d385315b25c883dc8
evgeny <69164923+velocode@users.noreply.github.com>
2022-06-01 18:13:52 +0200

    bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
+import userName from '../src/cli.js';
+
 console.log("Welcome to the Brain Games!");
+console.log('Hello ' + userName + '!');
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log(Math.floor(Math.random() * 100));
