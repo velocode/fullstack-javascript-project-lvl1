daa1fe32aa050de571655a2fa7c0f8c02d3ba793
evgeny <69164923+velocode@users.noreply.github.com>
2022-06-03 07:27:28 +0200

    games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,1 @@
 #!/usr/bin/env node
-import userName from '../src/cli.js';
-import readlineSync from 'readline-sync';
-
-console.log("Welcome to the Brain Games!");
-console.log('Hello ' + userName + '!');
