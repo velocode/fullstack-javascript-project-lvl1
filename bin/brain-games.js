cc56ced02db689957521a23aed9880af9905ba24
evgeny <69164923+velocode@users.noreply.github.com>
2022-06-03 06:35:16 +0200

    brain games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import userName from '../src/cli.js';
+import readlineSync from 'readline-sync';
 
 console.log("Welcome to the Brain Games!");
 console.log('Hello ' + userName + '!');
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-console.log(Math.floor(Math.random() * 100));
