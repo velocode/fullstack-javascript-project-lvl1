bb8dc770e48f5837050e1a54f1d3c5b684968e49
evgeny <69164923+velocode@users.noreply.github.com>
2022-06-07 20:05:03 +0200

    games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 /* eslint-disable linebreak-style */
-import userName from '../src/cli.js';
+import start from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-console.log(`Hello ${userName}!`);
+start();
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const userName = readlineSync.question('May I have your name? ');
+const start = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!`);
+};
 
-export default userName;
+export default start;
