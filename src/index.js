954c4561c808776952f41873900c3cde46fcfcbe
evgeny <69164923+velocode@users.noreply.github.com>
2022-06-11 08:54:41 +0200

    deleted jest

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const userName = readlineSync.question('May I have your name? ');
+const userName = readlineSync.question('May I have your name?');
 const start = () => {
   console.log('Welcome to the Brain Games!');
   console.log(`Hello ${userName}!`);
@@ -12,5 +12,5 @@ const correctAnswer = () => {
 const answer = readlineSync.question('Your answer: ');
 
 export {
-   start, correctAnswer, userName, answer 
+  start, correctAnswer, userName, answer,
 };
